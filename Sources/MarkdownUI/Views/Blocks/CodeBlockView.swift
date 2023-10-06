211b9235fc45cacf87df88e46a769e10523ff8a5
hoangnm <hoangnm.53@gmail.com>
2023-10-06 10:50:39 +0700

    Add copy button on top right

--- Sources/MarkdownUI/Views/Blocks/CodeBlockView.swift
+++ Sources/MarkdownUI/Views/Blocks/CodeBlockView.swift
@@ -6,7 +6,8 @@ struct CodeBlockView: View {
 
   private let fenceInfo: String?
   private let content: String
-//  @State private var onHover = false
+  @State private var onHover = false
+  @State var isCopied = false
 
   init(fenceInfo: String?, content: String) {
     self.fenceInfo = fenceInfo
@@ -14,38 +15,52 @@ struct CodeBlockView: View {
   }
 
   var body: some View {
-    self.codeBlock.makeBody(
-      configuration: .init(
-        language: self.fenceInfo,
-        content: self.content,
-        label: .init(self.label)
-      )
-    )
-//    .onHover { over in
-//        onHover = over
-//    }
+      ZStack(alignment: .top) {
+        self.codeBlock.makeBody(
+            configuration: .init(
+              language: self.fenceInfo,
+              content: self.content,
+              label: .init(self.label)
+            )
+        )
+        
+        HStack {
+            Spacer()
+            Button {
+                withAnimation(.linear(duration: 0.1)) {
+                    isCopied = true
+                }
+                #if os(macOS)
+                NSPasteboard.general.clearContents()
+                NSPasteboard.general.setString(content, forType: .string)
+                #else
+                UIPasteboard.general.string = content
+                #endif
+                
+                Task {
+                    try await Task.sleep(nanoseconds: 1_000_000_000)
+                    withAnimation(.linear(duration: 0.1)) {
+                        isCopied = false
+                    }
+                }
+            } label: {
+                Image(systemName: isCopied ? "checkmark" : "doc.on.clipboard")
+                    .font(.caption)
+                    .frame(width: 14, height: 14)
+            }
+            .foregroundColor(Color.accentColor)
+        }
+        .padding([.top, .trailing], 8)
+        .opacity(onHover == true ? 1 : 0)
+    }
+    .onHover { over in
+        onHover = over
+    }
   }
 
   private var label: some View {
     self.codeSyntaxHighlighter.highlightCode(self.content, language: self.fenceInfo)
       .textStyleFont()
       .textStyleForegroundColor()
-//      .overlay(
-//         Button {
-//            #if os(macOS)
-//            NSPasteboard.general.clearContents()
-//            NSPasteboard.general.setString(content, forType: .string)
-//            #else
-//            UIPasteboard.general.string = content
-//            #endif
-//         } label: {
-//           Image(systemName: "doc.on.doc")
-//             .font(.caption)
-//         }
-//         .foregroundColor(Color.accentColor)
-//         .offset(x: -14, y: 0)
-//         .opacity(onHover == true ? 1 : 0)
-//         ,alignment: .topTrailing
-//       )
   }
 }
